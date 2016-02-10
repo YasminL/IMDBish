6901399d17c5b8e5c9872f431fc15cee288bfd7f
YasminL <yasmin.lindholm@ffcg.se>
2016-02-10 18:53:08 +0100

    initial commit

---
+++ FFCG.G4.IMDB.Yasmin.Tests/MovieTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FFCG.G4.IMDB.Yasmin;
+
+namespace FFCG.G4.IMDB.Yasmin.Tests
+{
+    [TestFixture]
+    class MovieTests
+    {
+        private Movie _movie;
+
+        private const int MovieId = 1;
+        private const string MovieName = "Harry Potthead";
+
+        [SetUp]
+        public void SetUp()
+        {
+            _movie = new Movie(MovieId, MovieName);
+        }
+
+        [Test]
+        public void Movie_Is_Added_With_An_Id()
+        {
+            var actual = _movie.MovieId;
+            var expected = MovieId;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void Movie_Is_Added_With_A_Name()
+        {
+            var actual = _movie.MovieName;
+            var expected = MovieName;
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ FFCG.G4.IMDB.Yasmin/MovieCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFCG.G4.IMDB.Yasmin
+{
+    public class MovieCollection
+    {
+        private readonly List<Movie> _movies;
+        public IEnumerable<Movie> Movies { get {return _movies;} }
+
+        public MovieCollection()
+        {
+            _movies = new List<Movie>();
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            if (!_movies.Contains(movie))
+            {
+                _movies.Add(movie);
+            }
+        }
+
+        public List<Movie> GetAllMoviesInCollection()
+        {
+            return _movies;
+        }
+    }
+
+} 
